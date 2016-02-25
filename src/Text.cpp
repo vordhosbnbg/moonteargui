db20efe2b497a4e0909de3336dcf82cbfe9adc28
Boris Lazarov <vordhosbn.bg@gmail.com>
2016-02-25 19:01:55 +0200

    Added setters and getters for Text widget

--- src/Text.cpp
+++ src/Text.cpp
@@ -3,7 +3,7 @@
 
 #define DEFAULT_PT_SIZE 18
 using namespace std;
-Text::Text(shared_ptr<TextResource> defaultText, shared_ptr<FontResource> defaultFont) : sizePt(DEFAULT_PT_SIZE)
+Text::Text(shared_ptr<TextResource> defaultText, shared_ptr<FontResource> defaultFont) : sizePt(DEFAULT_PT_SIZE), fgColor({0xFF, 0xFF, 0xFF, 0xFF}), bgColor({ 0x00, 0x00, 0x00, 0x00 })
 {
     textRes = defaultText;
     fontRes = defaultFont;
@@ -13,10 +13,59 @@ Text::~Text()
 {
 }
 
+void Text::SetText(std::shared_ptr<TextResource> text)
+{
+    textRes = text;
+    cached = false;
+}
+
+std::shared_ptr<TextResource> Text::GetText()
+{
+    return textRes;
+}
+
+void Text::SetFont(std::shared_ptr<FontResource> font)
+{
+    fontRes = font;
+    cached = false;
+}
+
+std::shared_ptr<FontResource> Text::GetFont()
+{
+    return fontRes;
+}
+
 void Text::SetSizePt(int size)
 {
     sizePt = size;
     cached = false;
+}
+
+int Text::GetSizePt()
+{
+    return sizePt;
+}
+
+void Text::SetFGColor(SDL_Color col)
+{
+    fgColor = col;
+    cached = false;
+}
+
+SDL_Color Text::GetFGColor()
+{
+    return fgColor;
+}
+
+void Text::SetBGColor(SDL_Color col)
+{
+    bgColor = col;
+    cached = false;
+}
+
+SDL_Color Text::GetBGColor()
+{
+    return bgColor;
 }
 
 void Text::OnRegisterRenderer()
@@ -45,14 +94,12 @@ void Text::RenderText()
     int outline = 0;
     int hinting = TTF_HINTING_NORMAL;
     int kerning = 1;
-    SDL_Color foreground_color = {0xFF, 0xFF, 0xFF, 0};
-    SDL_Color background_color = {0x00, 0x00, 0x00, 0};
     TTF_Font * font_handle = fontRes->GetFontHandle();
     TTF_SetFontStyle(font_handle, renderstyle);
     TTF_SetFontOutline(font_handle, outline);
     TTF_SetFontKerning(font_handle, kerning);
     TTF_SetFontHinting(font_handle, hinting);
-    shared_ptr<SDLSurface> surface = make_shared<SDLSurface>(TTF_RenderText_Shaded(font_handle, textRes->GetString().c_str(), foreground_color, background_color));
+    shared_ptr<SDLSurface> surface = make_shared<SDLSurface>(TTF_RenderText_Shaded(font_handle, textRes->GetString().c_str(), fgColor, bgColor));
     sdlTexture = make_shared<SDLTexture>(sdlRenderer, surface);
     int width = sdlTexture->GetWidth();
     int height = sdlTexture->GetHeight();
--- src/Text.h
+++ src/Text.h
@@ -10,9 +10,22 @@ public:
     Text(std::shared_ptr<TextResource> defaultText, std::shared_ptr<FontResource> defaultFont);
     ~Text();
 
-    void SetSizePt(int size);
-    virtual void OnRegisterRenderer();
+    void SetText(std::shared_ptr<TextResource> text);
+    std::shared_ptr<TextResource> GetText();
 
+    void SetFont(std::shared_ptr<FontResource> font);
+    std::shared_ptr<FontResource> GetFont();
+
+    void SetSizePt(int size);
+    int GetSizePt();
+
+    void SetFGColor(SDL_Color col);
+    SDL_Color GetFGColor();
+
+    void SetBGColor(SDL_Color col);
+    SDL_Color GetBGColor();
+
+    virtual void OnRegisterRenderer();
     virtual void Draw();
 protected:
 
@@ -22,6 +35,8 @@ protected:
     std::shared_ptr<TextResource> textRes;
     std::shared_ptr<FontResource> fontRes;
     std::shared_ptr<SDLTexture> sdlTexture;
+    SDL_Color fgColor;
+    SDL_Color bgColor;
 };
 
 
